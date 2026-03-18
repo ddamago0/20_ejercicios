5b603f26fc7d06db3e93112f1ef58c0490e8ed8e
Daniel Martinez <ddamago0@gmail.com>
2026-03-18 12:55:37 -0500

    04/20

---
+++ 04.py
@@ -0,0 +1,11 @@
+edad = int (input("Ingrese su edad: "))
+
+if edad < 12:
+    print ("Valor de entrada: $8000.")
+
+elif edad >= 12 and edad <= 59:
+    print ("Valor de entrada: $12000.")
+
+else:
+    print ("Valor de entrada: $9000.")
+
